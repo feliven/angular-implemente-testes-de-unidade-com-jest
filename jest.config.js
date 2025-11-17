661c889591b0ef05f1aea072c3cdd84dd738ab11
FVS <feliven@gmail.com>
2025-11-17 13:44:30 -0300

    configura jest para acessar pasta public

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,25 @@
-module.exports = {
-  preset: 'jest-preset-angular',
-  globalSetup: 'jest-preset-angular/global-setup',
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+
+const config = {
+  preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+  coverageProvider: "v8",
+  moduleNameMapper: {
+    "^@app/(.*)$": "<rootDir>/src/app/$1",
+    "^@environments/(.*)$": "<rootDir>/src/environments/$1",
+  },
+  globals: {
+    "ts-jest": {
+      tsconfig: "<rootDir>/tsconfig.spec.json",
+      stringifyContentPathRegex: "\\.(html|svg)$",
+    },
+  },
 };
+
+module.exports = config;
