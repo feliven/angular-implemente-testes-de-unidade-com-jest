6c0ec4bc9b817eb343704118a06097918a95db42
FVS <feliven@gmail.com>
2025-11-17 14:56:11 -0300

    cria teste para alt e src no CabecalhoComponent

--- src/app/componentes/cabecalho/cabecalho.component.spec.ts
+++ src/app/componentes/cabecalho/cabecalho.component.spec.ts
@@ -1,17 +1,25 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CabecalhoComponent } from './cabecalho.component';
 
 describe('CabecalhoComponent', () => {
   let componente: CabecalhoComponent;
+  let fixture: ComponentFixture<CabecalhoComponent>;
 
   beforeEach(() => {
     TestBed.runInInjectionContext(() => {
       componente = new CabecalhoComponent();
     });
+    fixture = TestBed.createComponent(CabecalhoComponent);
+    componente = fixture.componentInstance;
   });
 
   it('deveria ser criado', () => {
     expect(componente).toBeTruthy();
+  });
+
+  it('deveria definir propriedades alt e src', () => {
+    expect(componente.alt).toBeDefined();
+    expect(componente.src).toBeDefined();
   });
 });
