7cb69f601075538b59060a0ea7cc6677dc9174c7
FVS <feliven@gmail.com>
2025-11-21 10:27:13 -0300

    crtia teste para adicionarLivro() em FormularioComponent

--- src/app/paginas/formulario/formulario.component.spec.ts
+++ src/app/paginas/formulario/formulario.component.spec.ts
@@ -34,4 +34,41 @@ describe('FormularioComponent', () => {
       classificacao: null,
     });
   });
+
+  it('deveria adicionar um novo livro', () => {
+    const novoLivro = {
+      titulo: 'Novo Livro',
+      autoria: 'Autoria Desconhecida',
+      imagem: 'http://example.com/cover.jpg',
+      genero: 'romance',
+      dataLeitura: '2024-04-19',
+      classificacao: 5,
+    };
+
+    const adicionarLivroServiceSpy = jest.spyOn(service, 'adicionarLivro');
+    const routerSpy = jest.spyOn(componente['router'], 'navigate');
+
+    componente.formulario.setValue(novoLivro);
+    componente.adicionarLivro();
+
+    expect(adicionarLivroServiceSpy).toHaveBeenCalledWith({
+      ...novoLivro,
+      genero: componente.generos.find((g) => g.id === novoLivro.genero),
+    });
+
+    expect(componente.formulario.value).toEqual({
+      titulo: null,
+      autoria: null,
+      imagem: null,
+      genero: null,
+      dataLeitura: null,
+      classificacao: null,
+    });
+
+    expect(routerSpy).toHaveBeenCalledWith(['lista-livros']);
+  });
+
+  //   it('', () => {});
+  //   it('', () => {});
+  //   it('', () => {});
 });
